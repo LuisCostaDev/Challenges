daeb972554d9aa9a76b8d84f343c2d2272a2bd31
LuisCosta <bettermeyesican@gmail.com>
2021-11-03 15:34:43 +0100

    Create Sum_N_first_Numbers.c

---
+++ C/Sum_N_first_Numbers.c
@@ -0,0 +1,20 @@
+/* ******************************************************************/
+/*                                                                  */                                                          
+/*   Sum_N_first_Numbers.c                                          */                                                        
+/*   By: LuisCosta                           				        */
+/*                                                                  */  
+/* ******************************************************************/
+#include <stdio.h>
+int main()
+{
+    int number, i = 1, result = 0;
+    printf("Calculo da soma dos primeiros N numeros.\nIndique N:");
+    scanf("%d", &number);
+    while (i <= number){
+        result += i;
+        printf("\n adicionar %d, parcial %d", i, result);
+     i++;
+    }
+    printf("\nTotal: %d\n", result);
+}
+
